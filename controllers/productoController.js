e661ed86250b2d850603e1fcfd4181e0c9943700
GeronimoDavis <geronimodavis@gmail.com>
2025-08-05 15:02:39 -0300

    Validaciones en productoController

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,10 +1,34 @@
 import ProductoModel from "../models/productoModel.js";
 import CategoriaProducto from "../models/categoriaProductoModel.js";
+import mongoose from "mongoose";
 
 
 export const createProducto = async (req, res) =>{
     try{
         const { nombre, stock, precioVenta, precioCompra, categoriaProducto } = req.body;
+
+        //validaciones 
+        
+        if(!nombre || typeof nombre !== "string" || nombre.trim() === " "){
+            return res.status(400).json({ message: "El nombre del producto es obligatorio y debe ser una cadena de texto válida" });
+        }
+
+        if(stock == null || isNaN(stock) || stock < 0){
+            return res.status(400).json({ message: "El stock del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
+
+        if(precioVenta == null || isNaN(precioVenta) || precioVenta < 0){
+            return res.status(400).json({ message: "El precio de venta del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
+
+        if(precioCompra == null || isNaN(precioCompra) || precioCompra < 0){
+            return res.status(400).json({ message: "El precio de compra del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
+
+        if(!categoriaProducto || !mongoose.Types.ObjectId.isValid(categoriaProducto)){
+            return res.status(400).json({ message: "ID de la categoría no válido" });
+        }
+
         const existe = await ProductoModel.findOne({ nombre });
         if(existe){
             return res.status(400).json({ message: "El producto ya existe" });
@@ -42,6 +66,10 @@ export const getAllProductos = async (req, res) => {
 export const getProductoById = async (req, res) => {
     try{
         const { id } = req.params;
+        //validaciones
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({ message: "ID de producto no válido" });
+        }
         const producto = await ProductoModel.findById(id).populate('categoriaProducto');
         if(!producto){
             return res.status(404).json({ message: "Producto no encontrado" });
@@ -55,7 +83,34 @@ export const getProductoById = async (req, res) => {
 export const updateProducto = async (req, res) => {
     try{
         const {id} = req.params;
+
+        //validaciones
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({ message: "ID de producto no válido" });
+        }
+
         const { nombre, stock, precioVenta, precioCompra, categoriaProducto } = req.body;
+
+        //validaciones
+
+        if(!nombre || typeof nombre !== "string" || nombre.trim() === " "){
+            return res.status(400).json({ message: "El nombre del producto es obligatorio y debe ser una cadena de texto válida" });
+        }
+
+        if(stock == null || isNaN(stock) || stock < 0){
+            return res.status(400).json({ message: "El stock del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
+        
+        if(precioVenta == null || isNaN(precioVenta) || precioVenta < 0){
+            return res.status(400).json({ message: "El precio de venta del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
+
+        if(precioCompra == null || isNaN(precioCompra) || precioCompra < 0){
+            return res.status(400).json({ message: "El precio de compra del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
+        if(!categoriaProducto || !mongoose.Types.ObjectId.isValid(categoriaProducto)){
+            return res.status(400).json({ message: "ID de la categoría no válido" });
+        }
 
         const existe = await ProductoModel.findOne({ nombre });
         if(existe && existe._id.toString() !== id){
@@ -84,6 +139,14 @@ export const updateStockProducto = async (req, res) =>{
     try{
         const {id} = req.params;
         const { stock } = req.body;
+        //validaciones
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({ message: "ID de producto no válido" });
+        }
+
+        if(stock == null || isNaN(stock) || stock < 0){
+            return res.status(400).json({ message: "El stock del producto es obligatorio y debe ser un número mayor o igual a 0" });
+        }
         const productoActualizado = await ProductoModel.findByIdAndUpdate(id, { stock }, { new: true }).populate('categoriaProducto');
         res.status(200).json(productoActualizado);
     }catch(err){
@@ -96,6 +159,10 @@ export const updateStockProducto = async (req, res) =>{
 export const getProductosByCategoria = async (req, res) => {
   try {
     const { idCategoria } = req.params;
+    //validaciones 
+    if(!mongoose.Types.ObjectId.isValid(idCategoria)){
+        return res.status(400).json({ message: "ID de categoría no válido" });
+    }
     const existe = await CategoriaProducto.findById(idCategoria);
     if(!existe){
         return res.status(400).json({ message: "La categoría del producto no existe" });
