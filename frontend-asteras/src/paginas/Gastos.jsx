d8b2772968447bc9ec5ba9ed5fa4b2450e73a138
GeronimoDavis <geronimodavis@gmail.com>
2025-08-27 10:48:21 -0300

    handleCancel añadido en gastos

--- frontend-asteras/src/paginas/Gastos.jsx
+++ frontend-asteras/src/paginas/Gastos.jsx
@@ -1,168 +1,192 @@
 import { useEffect, useState } from "react";
-import { getAllGastos, getGastoByid, createGasto, updateGastos, getCategoriasGastos } from "../api";
+import {
+  getAllGastos,
+  getGastoByid,
+  createGasto,
+  updateGastos,
+  getCategoriasGastos,
+} from "../api";
 
-const Gastos = () =>{   
+const Gastos = () => {
+  const [gastos, setGastos] = useState([]);
+  const [error, setError] = useState("");
+  const [editId, setEditId] = useState(null);
+  const [formData, setFormData] = useState({
+    descripcion: "",
+    monto: "",
+    categoria: "",
+  });
+  const [categoria, setCategoria] = useState([]);
 
-    const [gastos, setGastos] = useState([]);
-    const [error, setError] = useState("")
-    const [editId, setEditId] = useState(null)
-    const [formData, setFormData] = useState({
-        descripcion:"",
-        monto:"",
-        categoria:""
-    })
-    const [categoria, setCategoria] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resGasto = await getAllGastos();
+        setGastos(resGasto.data);
 
-    useEffect(() => {
-        const fetchData = async () =>{
-            try{
-                const resGasto = await getAllGastos();
-                setGastos(resGasto.data);
-
-                const resCategoria = await getCategoriasGastos();
-                setCategoria(resCategoria.data);
-            }catch(err){
-                setError("Error al cargar datos");
-                console.error(err);
-            }
-        };
-
-        fetchData();
-
-    },[]);
-
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        if(editId){
-            try{
-                const data = {
-                    ...formData,
-                    monto: parseFloat(formData.monto)
-                };
-
-                await updateGastos(editId, data);
-                setEditId(null);
-            }catch(err){
-                setError("Error al editar los datos");
-                console.error(err);
-            }
-        }else{
-            try{
-                const data = {
-                    ...formData,
-                    monto: parseFloat(formData.monto)
-                };
-
-                await createGasto(data);
-            }catch(err){
-                setError("Error al crear registro");
-                console.error(err);
-            }
-        }
-
-        setFormData({
-            descripcion: "",
-            monto: "",
-            categoria:""
-        });
-        try{ 
-            const resGasto = await getAllGastos();
-            setGastos(resGasto.data);
-        }catch(err){
-            setError("Error al cargar datos");
-            console.error(err);
-        }
-            
-    } 
-
-    const handleEdit = (gasto) =>{
-        setEditId(gasto._id);
-        setFormData({
-            descripcion: gasto.descripcion,
-            monto: gasto.monto,
-            categoria: gasto.categoria?._id
-        });
-    }
-
-    const handleChange = (e) =>{
-        setFormData({ ...formData, [e.target.name]: e.target.value});
+        const resCategoria = await getCategoriasGastos();
+        setCategoria(resCategoria.data);
+      } catch (err) {
+        setError("Error al cargar datos");
+        console.error(err);
+      }
     };
 
-    return(
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (editId) {
+      try {
+        const data = {
+          ...formData,
+          monto: parseFloat(formData.monto),
+        };
+
+        await updateGastos(editId, data);
+        setEditId(null);
+      } catch (err) {
+        setError("Error al editar los datos");
+        console.error(err);
+      }
+    } else {
+      try {
+        const data = {
+          ...formData,
+          monto: parseFloat(formData.monto),
+        };
+
+        await createGasto(data);
+      } catch (err) {
+        setError("Error al crear registro");
+        console.error(err);
+      }
+    }
+
+    setFormData({
+      descripcion: "",
+      monto: "",
+      categoria: "",
+    });
+    try {
+      const resGasto = await getAllGastos();
+      setGastos(resGasto.data);
+    } catch (err) {
+      setError("Error al cargar datos");
+      console.error(err);
+    }
+  };
+
+  const handleEdit = (gasto) => {
+    setEditId(gasto._id);
+    setFormData({
+      descripcion: gasto.descripcion,
+      monto: gasto.monto,
+      categoria: gasto.categoria?._id,
+    });
+  };
+
+  const handleCancel = () => {
+    setEditId(null);
+    setFormData({
+      descripcion: "",
+      monto: "",
+      categoria: "",
+    });
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  if (error) return <p>{error}</p>;
+
+  return (
     <div>
-        <h2>Registrar Gasto</h2>
-        <form onSubmit={handleSubmit}>
-            <select 
-                name="categoria"
-                value={formData.categoria}
-                onChange={handleChange}
-                required
-            >
-                    <option value="">Seleccionar una categoria</option>
-                    {categoria.map((c) => (
-                        <option key={c._id} value={c._id}>
-                            {c.nombre}
-                        </option>
-                    ))}
-            </select>
-            
-            <input 
-                type="text" 
-                name="descripcion"
-                placeholder="Descripcion"
-                value={formData.descripcion}
-                onChange={handleChange}
-                required
-            />
+      <h2>Registrar Gasto</h2>
+      <form onSubmit={handleSubmit}>
+        <select
+          name="categoria"
+          value={formData.categoria}
+          onChange={handleChange}
+          required
+        >
+          <option value="">Seleccionar una categoria</option>
+          {categoria.map((c) => (
+            <option key={c._id} value={c._id}>
+              {c.nombre}
+            </option>
+          ))}
+        </select>
 
-            <input 
-                type="number"
-                name="monto"
-                placeholder="Monto"
-                value={formData.monto}
-                onChange={handleChange}
-                required
-            />
+        <input
+          type="text"
+          name="descripcion"
+          placeholder="Descripcion"
+          value={formData.descripcion}
+          onChange={handleChange}
+          required
+        />
 
-            <button type="submit">
-                {editId ? "Actualizar gasto" : "Registrar gasto"}
-            </button>
-        </form>
-        
-        <h2>lista de gastos</h2>
-        <div>
-            {gastos.length === 0 ? (
-                <p> No hay gastos registradas</p>
-            ) : (
-                <dic>
-                    <table className="ventas-table">
-                        <thead>
-                            <tr>
-                            <th>Descripcion</th>
-                            <th>Monto</th>
-                            <th>Categoria</th>
-                            <th>Fecha</th>
-                            <th>Accion</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {gastos.map((g) => (
-                                <tr key={g._id}>
-                                    <td>{g.descripcion}</td>
-                                    <td>{g.monto}</td>
-                                    <th>{g.categoria?.nombre || "No hay categoria"}</th>
-                                    <td>{new Date(g.fecha).toLocaleString()}</td>
-                                    <td><button onClick={() => handleEdit(g)} className="btn-editar">Editar</button></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                </table>
-                </dic>
-            )} 
-        </div>
+        <input
+          type="number"
+          name="monto"
+          placeholder="Monto"
+          value={formData.monto}
+          onChange={handleChange}
+          required
+        />
 
+        <button type="submit">
+          {editId ? "Actualizar gasto" : "Registrar gasto"}
+        </button>
+        {editId && (
+          <button type="button" className="btn-cancelar" onClick={handleCancel}>
+            Cancelar Edición
+          </button>
+        )}
+      </form>
+
+      <h2>lista de gastos</h2>
+      <div>
+        {gastos.length === 0 ? (
+          <p> No hay gastos registradas</p>
+        ) : (
+          <dic>
+            <table className="ventas-table">
+              <thead>
+                <tr>
+                  <th>Descripcion</th>
+                  <th>Monto</th>
+                  <th>Categoria</th>
+                  <th>Fecha</th>
+                  <th>Accion</th>
+                </tr>
+              </thead>
+              <tbody>
+                {gastos.map((g) => (
+                  <tr key={g._id}>
+                    <td>{g.descripcion}</td>
+                    <td>{g.monto}</td>
+                    <th>{g.categoria?.nombre || "No hay categoria"}</th>
+                    <td>{new Date(g.fecha).toLocaleString()}</td>
+                    <td>
+                      <button
+                        onClick={() => handleEdit(g)}
+                        className="btn-editar"
+                      >
+                        Editar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </dic>
+        )}
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default Gastos;
